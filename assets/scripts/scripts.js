9408e7d0c2eaaf5dfaa453eefdb3baca6130be16
John Peart <code@johnpe.art>
2021-04-06 22:24:28 +0000

    Add about page styles And fix relates issues

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,4 +6,9 @@ function toggleMenu() {
   var menu = document.getElementById("header-menu-items");
   menu.classList.toggle("open");
   menu.classList.toggle("closed");
+  if (menu.classList.contains("open")) {
+    menu.setAttribute('aria-expanded', 'true')
+  } else {
+    menu.setAttribute('aria-expanded', 'false')
+  }
 }
