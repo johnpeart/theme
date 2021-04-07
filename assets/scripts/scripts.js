af15500e52040f1cb2738679c812756071725d31
John Peart <code@johnpe.art>
2021-04-07 08:57:09 +0000

    Update header

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,14 +1,28 @@
-function toggleMenu() {
-  var menu = document.getElementById("header-menu-toggle");
-  menu.classList.toggle("open");
-  menu.classList.toggle("closed");
+var menuToggle = document.getElementById("header-menu-toggle");
+var menuItems = document.getElementById("header-menu-items");
+
+menuToggle.addEventListener("click", function() {
   
-  var menu = document.getElementById("header-menu-items");
-  menu.classList.toggle("open");
-  menu.classList.toggle("closed");
-  if (menu.classList.contains("open")) {
-    menu.setAttribute('aria-expanded', 'true')
+  this.classList.toggle("open");
+  this.classList.toggle("closed");
+  
+  if (this.classList.contains("open")) {
+    this.setAttribute("aria-expanded", "true");
   } else {
-    menu.setAttribute('aria-expanded', 'false')
+    this.setAttribute("aria-expanded", "false");
   }
-}
+  
+  menuItems.classList.toggle("open");
+  menuItems.classList.toggle("closed");
+  
+});
+
+// function toggleMenu() {
+//   var menu = document.getElementById("header-menu-toggle");
+//   menu.classList.toggle("open");
+//   menu.classList.toggle("closed");
+//   
+//   var menu = document.getElementById("header-menu-items");
+//   menu.classList.toggle("open");
+//   menu.classList.toggle("closed");
+// }
